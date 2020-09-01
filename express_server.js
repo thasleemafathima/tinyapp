893b1dfc17e85686b0b1652184552d54e2eb640a
Thasleema Fathima Puroskhan <thasleemafathima92@gmail.com>
2020-09-01 19:40:41 +0000

    post

--- express_server.js
+++ express_server.js
@@ -2,12 +2,28 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+function generateRandomString() {
+
+  let result = '';
+  let length = 6;
+  let chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  for (var i = length; i > 0; --i) {
+    result += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return result;
+}
+
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -45,7 +61,21 @@ app.get("/hello", (req, res) => {
   res.render("hello_world", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+
+app.post("/urls", (req, res) => {
+
+    // Log the POST request body to the console
+  let shortu = generateRandomString();
+  urlDatabase[shortu] = req.body.longURL;
+  res.redirect(`/urls/${shortu}`)
+  //res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
